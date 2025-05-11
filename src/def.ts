e9dbfe7df12141f0bf1525229c392b32d9659d3a
zsh2401 <zsh2401@163.com>
2025-05-11 15:21:45 +0800

    object type

--- src/def.ts
+++ src/def.ts
@@ -2,43 +2,47 @@ import { z } from 'zod'
 
 export type ZodBase = z.ZodObject<any, any, any, any>
 export type API<DTO extends ZodBase, VO extends ZodBase> =
-  | FullAPI<DTO, VO>
-  | NoArgAPI<VO>
-  | NoRetAPI<DTO>
-  | NoIOApi
+    | FullAPI<DTO, VO>
+    | NoArgAPI<VO>
+    | NoRetAPI<DTO>
+    | NoIOApi
 
 export interface FullAPI<
-  DTO extends ZodBase,
-  VO extends ZodBase,
+    DTO extends ZodBase,
+    VO extends ZodBase,
 > {
-  path: string
-  dtoSchema: DTO
-  voSchema: VO
+    path: string
+    dtoSchema: DTO
+    voSchema: VO
 }
 
 export interface NoRetAPI<DTO extends ZodBase> {
-  path: string
-  dtoSchema: DTO
+    path: string
+    dtoSchema: DTO
+    voSchema?: void
 }
 export interface NoArgAPI<VO extends ZodBase> {
-  path: string
-  voSchema: VO
+    path: string
+    dtoSchema?: void
+    voSchema: VO
 }
 export interface NoIOApi {
-  path: string
+    path: string
+    dtoSchema?: void
+    voSchema?: void
 }
 
 export function defineAPI<
-  DTO extends ZodBase,
-  VO extends ZodBase,
+    DTO extends ZodBase,
+    VO extends ZodBase,
 >(def: FullAPI<DTO, VO>): FullAPI<DTO, VO>
 export function defineAPI<DTO extends ZodBase>(
-  def: NoRetAPI<DTO>
+    def: NoRetAPI<DTO>
 ): NoRetAPI<DTO>
 export function defineAPI<VO extends ZodBase>(
-  def: NoArgAPI<VO>
+    def: NoArgAPI<VO>
 ): NoArgAPI<VO>
 export function defineAPI(def: NoIOApi): NoIOApi
 export function defineAPI(def: any) {
-  return def
+    return def
 }
--- src/server/express.ts
+++ src/server/express.ts
@@ -1,131 +1,133 @@
 import type { Express, Request } from 'express'
 import { z } from 'zod'
 import type {
-  API,
-  FullAPI,
-  NoArgAPI,
-  NoIOApi,
-  NoRetAPI,
-  ZodBase,
+    API,
+    FullAPI,
+    NoArgAPI,
+    NoIOApi,
+    NoRetAPI,
+    ZodBase,
 } from '@/def'
 import { ofError, ofSuccess, type Result } from '@/Result'
 
 /**
  * The args of express handler implementation
  */
-export interface ExpressHandlerArgs<DTO> {
-  request: Express.Request
-  response: Express.Response
-  dto: DTO
-}
-export type ExpressAPIHandler<T extends API<any, any>> =
-  T extends FullAPI<infer DTO, infer VO>
-    ? (
-        args: ExpressHandlerArgs<T['dtoSchema']>
-      ) => Promise<T['voSchema']>
-    : T extends NoRetAPI<infer VO>
-      ? (
-          dto: ExpressHandlerArgs<T['dtoSchema']>
-        ) => Promise<void>
-      : T extends NoArgAPI<infer VO>
-        ? () => T['voSchema']
-        : () => Promise<void>
+export type HandlerArgs<DTO extends ZodBase | void> =
+    DTO extends ZodBase ?
+    {
+        request: Express.Request
+        response: Express.Response
+        dto: z.output<DTO>
+    }
+    :
+    {
+        request: Express.Request
+        response: Express.Response
+    }
 
-type NotAllowed<T, U> = T extends U ? never : T
+export type HandlerResult<VO extends ZodBase | void> =
+    VO extends ZodBase ?
+    Promise<z.output<VO>> : Promise<void>
+
+export interface Handler<DTO extends ZodBase | void, VO extends ZodBase | void> {
+    (args: HandlerArgs<DTO>): HandlerResult<VO>
+}
 
 /**
  * Implement your api on express instance
  */
-export function implAPIOnExpress<
-  DTO extends ZodBase,
-  VO extends ZodBase,
+export function implApi<
+    DTO extends ZodBase,
+    VO extends ZodBase,
 >(
-  express: Express,
-  api: FullAPI<DTO, VO>,
-  handler: ExpressAPIHandler<
-    FullAPI<z.output<DTO>, z.output<VO>>
-  >
+    express: Express,
+    api: FullAPI<DTO, VO>,
+    handler: Handler<DTO, VO>
 ): void
 
 /**
  * Implement your no argument api on express instance
  */
-export function implAPIOnExpress<VO extends ZodBase>(
-  express: Express,
-  api: NoArgAPI<VO>,
-  handler: ExpressAPIHandler<NoArgAPI<z.output<VO>>>
+export function implApi<VO extends ZodBase>(
+    express: Express,
+    api: NoArgAPI<VO>,
+    handler: Handler<void, VO>
 ): void
 /**
  * Implement your no returning api on express instance
  */
-export function implAPIOnExpress<DTO extends ZodBase>(
-  express: Express,
-  api: NoRetAPI<DTO>,
-  handler: ExpressAPIHandler<NoRetAPI<z.output<DTO>>>
+export function implApi<DTO extends ZodBase>(
+    express: Express,
+    api: NoRetAPI<DTO>,
+    handler: Handler<DTO, void>
 ): void
+
 
 /**
- * Implement your no argument and returning api on express instance
+ * Implement your no returning api on express instance
  */
-export function implAPIOnExpress<A extends NoIOApi>(
-  express: Express,
-  api: NotAllowed<A, NoArgAPI<any>>,
-  handler: (dto: ExpressHandlerArgs<void>) => Promise<void>
+export function implApi(
+    express: Express,
+    api: NoIOApi,
+    handler: Handler<void, void>
 ): void
 
-export function implAPIOnExpress(
-  express: any,
-  api: any,
-  handler: any
-): void {
-  if (api.path.toLowerCase() !== api.path) {
-    throw new Error('API path must be all lowercase')
-  }
-  express.post(api.path, async (request, response) => {
-    const args: ExpressHandlerArgs<z.output<any>> = {
-      request,
-      response,
-      dto: {} as z.output<any>,
+
+export function implApi(
+    express: Express,
+    api: any,
+    handler: any
+):
+    void {
+    if (api.path.toLowerCase() !== api.path) {
+        throw new Error('API path must be all lowercase')
     }
-    let result: Result<any>
-    try {
-      if (api.dtoSchema) {
-        if (
-          request.headers['content-type'] ===
-          'application/json'
-        ) {
-          const json: string =
-            await readRequestBodyAsString(request)
-          const obj = JSON.parse(json)
-          await (api.dtoSchema as ZodBase).parseAsync(obj)
-          args.dto = obj
+    express.post(api.path, async (request, response) => {
+        const args: HandlerArgs<z.output<any>> = {
+            request,
+            response,
+            dto: {} as z.output<any>,
         }
-      }
-      result = ofSuccess(await handler(args))
-    } catch (err: unknown) {
-      result = ofError(err)
-    }
-    const str = JSON.stringify(result)
-    response.setHeader('Content-Type', 'application/json')
-    response.send(str)
-  })
+        let result: Result<any>
+        try {
+            if (api.dtoSchema) {
+                if (
+                    request.headers['content-type'] ===
+                    'application/json'
+                ) {
+                    const json: string =
+                        await readRequestBodyAsString(request)
+                    const obj = JSON.parse(json)
+                    await (api.dtoSchema as ZodBase).parseAsync(obj)
+                    args.dto = obj
+                }
+            }
+            result = ofSuccess(await handler(args))
+        } catch (err: unknown) {
+            result = ofError(err)
+        }
+        const str = JSON.stringify(result)
+        response.setHeader('Content-Type', 'application/json')
+        response.send(str)
+    })
 }
 
 async function readRequestBodyAsString(
-  request: Request
+    request: Request
 ): Promise<string> {
-  return new Promise<string>((resolve) => {
-    let data = ''
+    return new Promise<string>((resolve) => {
+        let data = ''
 
-    request.on('readable', () => {
-      let chunk
-      while ((chunk = request.read()) !== null) {
-        data += chunk
-      }
-    })
+        request.on('readable', () => {
+            let chunk
+            while ((chunk = request.read()) !== null) {
+                data += chunk
+            }
+        })
 
-    request.on('end', () => {
-      resolve(data)
+        request.on('end', () => {
+            resolve(data)
+        })
     })
-  })
 }
